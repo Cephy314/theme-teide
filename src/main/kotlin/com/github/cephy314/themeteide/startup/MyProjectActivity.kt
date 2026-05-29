4069cd5d33d118d7fe93af3136847dccbb6e8b07
GitHub Action <action@github.com>
2026-05-29 01:42:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/cephy314/themeteide/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/cephy314/themeteide/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.cephy314.themeteide.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
